a0f22a7be8393cda486ec6e538bccab6aa45bda0
lovenoel <kinglovenoel@gmail.com>
2024-06-14 21:26:40 -0700

    tasks

--- models/state.py
+++ models/state.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.city import city
+from os import getenv
 
 
 class State(BaseModel):
     """ State class """
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
+    if getenv('HBNB_TYPE_STORAGE') != 'db':
+        @property
+        def cities(self):
+            """Getter method for cities when storage type is not db"""
+            from models import storage
+            city_list = []
+            for city in storage.all(city).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
